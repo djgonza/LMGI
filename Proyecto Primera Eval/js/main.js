26f1d5847b980c702d2d09f68e2ad7f153abe8ec
David Jimenez Gonzalez <davidjimenezgonza@gmail.com>
2015-10-10 11:54:55 +0200

    Estructura
    Mostrar y ocultar barra de navegación según la resolución

--- Proyecto Primera Eval/js/main.js
+++ Proyecto Primera Eval/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
 
-	/* navBar */
+	function ocultarNavBar () {
+		$("#navBar-back").css("height", "0");
+		if($(window).width() < 992)
+			$("#navBar").css("left", "-105%");
+	}
 
+	function mostrarNavBar () {
+		$("#navBar").css("left", 0);
+		$("#navBar-back").css("height", "100%");
+	}
+
+	/* navBar */
+	$("#buttonNav").click(function() {
+		mostrarNavBar ();
+	});
+
+	$("#navBar-back").click(function () {
+		ocultarNavBar ();
+	});
 
 	/* Secciones */
 
@@ -13,38 +30,42 @@ $(document).ready(function() {
 		$("#link-inicio").click(function() {
 			$("#seccion").load("secciones/inicio.html");
 			$("#titulo-seccion>span").html("Inicio");
+			ocultarNavBar ();
 		});
 		//Galeria
 		$("#link-galeria").click(function() {
 			$("#seccion").load("secciones/galeria.html");
 			$("#titulo-seccion>span").html("Galeria");
+			ocultarNavBar ();
 		});
 		//Contacto
 		$("#link-contacto").click(function() {
 			$("#seccion").load("secciones/contacto.html");
 			$("#titulo-seccion>span").html("Contacto");
-			console.log($("#seccion").load("contacto.html"));
+			ocultarNavBar ();
 		});
 		//Historia
 		$("#link-historia").click(function() {
 			$("#seccion").load("secciones/historia.html");
 			$("#titulo-seccion>span").html("Historia");
+			ocultarNavBar ();
 		});
 		//Peliculas
 		$("#link-peliculas").click(function() {
 			$("#seccion").load("secciones/peliculas.html");
 			$("#titulo-seccion>span").html("Peliculas");
+			ocultarNavBar ();
 		});
 		//Comics
 		$("#link-comics").click(function() {
 			$("#seccion").load("secciones/comics.html");
 			$("#titulo-seccion>span").html("Comics");
+			ocultarNavBar ();
 		});
 
 	/* Botones */
-	$("#buttonNav").click(function() {
-		$("#navBar").css("left", 0);
-		$("#navbar").css("padding-left", 240px)
-	});
+	
+
+
 
 });
