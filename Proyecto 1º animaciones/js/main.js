29740a487da726461bafcd9e5ed7e1ae70de4196
David Jimenez Gonzalez <davidjimenezgonza@gmail.com>
2015-10-26 23:42:24 +0100

    Actualización proyecto animaciones

---
+++ Proyecto 1º animaciones/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+	seccion = "";
+
+	function mostrarTituloSeccion (texto) {
+		$("#tituloSeccion").css("animation", "mostrarTituloSeccion 3s forwards");
+		$("#tituloSeccion").html(texto);
+
+	}
+
+	function ocultarTituloSeccion (texto) {
+		$("#tituloSeccion").css("animation", "ocultarTituloSeccion 3s forwards");
+		window.setTimeout(function () {
+			mostrarTituloSeccion (texto);
+		}, 2000);
+	}
+
+	function clickIcono (nombreSeccion) {
+		if (seccion == "") {
+			navIconos ();
+			mostrarTituloSeccion (nombreSeccion);
+		}else if (seccion != nombreSeccion){
+			ocultarTituloSeccion(nombreSeccion);
+		}
+		seccion = nombreSeccion;
+	}
+
+
+	//inicia la animacion de la barra de navegacion
+	function navIconos () {
+		$("#iconoComics").css("animation", "navIconoComic 3s forwards");
+		$("#iconoContacto").css("animation", "navIconoContacto 3s forwards");
+		$("#iconoPeliculas").css("animation", "navIconoPeliculas 3s forwards");
+		$("#iconoGaleria").css("animation", "navIconoGaleria 3s forwards");
+		$("#iconoHistoria").css("animation", "navIconoHistoria 3s forwards");
+
+		$("#navegacion").css("animation", "navegacion 3s forwards");
+
+	}
+
+	$("#iconoComics").click(function() {
+		clickIcono ("Comics");
+	});
+	$("#iconoContacto").click(function() {
+		clickIcono ("Contacto");
+	});
+	$("#iconoPeliculas").click(function() {
+		clickIcono ("Peliculas");
+	});
+	$("#iconoGaleria").click(function() {
+		clickIcono ("Galeria");
+	});
+	$("#iconoHistoria").click(function() {
+		clickIcono ("Historia");
+	});
+
+
+});
